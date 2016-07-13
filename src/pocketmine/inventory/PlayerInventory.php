438fe3c9723dbcb83a13609f5ab6ad1d68c7f319
Dylan K. Taylor <odigiman@gmail.com>
2016-07-13 14:19:46 +0100

    Fix hotbar slot mapping for Win10
    This will cripple Pocket Edition, but that's a worry for later.

--- src/pocketmine/inventory/PlayerInventory.php
+++ src/pocketmine/inventory/PlayerInventory.php
@@ -58,9 +58,9 @@ class PlayerInventory extends BaseInventory{
 	}
 
 	public function setHotbarSlotIndex($index, $slot){
-		if ($index >= 0 and $index < $this->getHotbarSize() and $slot >= -1 and $slot < $this->getSize()) {
+		/*if ($index >= 0 and $index < $this->getHotbarSize() and $slot >= -1 and $slot < $this->getSize()) {
 			$this->hotbar[$index] = $slot;//calculate has been done by client
-		}
+		}*/
 	}
 
 	public function getHeldItemIndex(){
@@ -414,7 +414,7 @@ class PlayerInventory extends BaseInventory{
 			if($player === $this->getHolder()){
 				for($i = 0; $i < $this->getHotbarSize(); ++$i){
 					$index = $this->getHotbarSlotIndex($i);
-					$pk->hotbar[] = $index <= -1 ? -1 : $index + $this->getHotbarSize();
+					$pk->hotbar[] = $index + $this->getHotbarSize();
 				}
 			}
 			if(($id = $player->getWindowId($this)) === -1 or $player->spawned !== true){
